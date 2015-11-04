2983ad41582774b18b5f1da35e621dbccb3a5b01
UltanGriffin <u.griffin1@nuigalway.ie>
2015-11-04 14:01:32 +0000

    no failures

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -44,7 +44,7 @@ rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   def update
     respond_to do |format|
       if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
+        format.html { redirect_to store_path(:cart), notice: 'Cart was successfully updated.' }
         format.json { render :show, status: :ok, location: @cart }
       else
         format.html { render :edit }
